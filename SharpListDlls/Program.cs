de783b4725f03d89fb1bb546582b4f2dd6a8c5bd
SpookySec <anonymousbot100110@gmail.com>
2021-07-25 13:58:51 +0300

    init commit

---
+++ SharpListDlls/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace SharpListDlls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Getting a current process object
+            Process localProc = Process.GetCurrentProcess();
+
+            // Process#Id returns the PID of the proc
+            Console.WriteLine("Listing loaded modules inside process PID: {0}", localProc.Id);
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+
+            // Looping and printing all loaded modules using Process#Modules
+            foreach (ProcessModule module in localProc.Modules)
+            {
+                Console.WriteLine("{0} is loaded @ 0x{1}", module.ModuleName, module.BaseAddress.ToString("X4"));
+            }
+        }
+    }
+}
